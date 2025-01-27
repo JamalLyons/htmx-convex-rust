0c462c3b194f6f5764a21ed0cc0bdc7c12377d88
Jamal <43446907+JamalLyons@users.noreply.github.com>
2025-01-27 13:45:45 -0500

    base code

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use lazy_static::lazy_static;
+use tera::Tera;
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let source = "templates/**/*";
+        let tera = Tera::new(source).unwrap();
+        tera
+    };
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    let context = tera::Context::new();
+    // context.insert("message_from_rust", "Hello from Rust!");
+    let page_content = TEMPLATES.render("index.html", &context).unwrap();
+    HttpResponse::Ok().body(page_content)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
